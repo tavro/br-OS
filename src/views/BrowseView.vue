c6592bed3585cf0c5856f012209fdec5e5407de5
Isak Horvath <isakhorvath@gmail.com>
2023-07-11 22:22:47 +0200

    brOS-001: Display catalogs and files from mock data

--- src/views/BrowseView.vue
+++ src/views/BrowseView.vue
@@ -5,8 +5,12 @@
                 <RouterLink to="/"><h1>user</h1></RouterLink>
             </div>
             <div class="filesystem">
-                <div v-for="file in fileList" class="catalog">
-                    <img class="icon" :src="icon"/>
+                <div v-for="catalog in catalogs" class="catalog">
+                    <img class="icon" :src="folderIcon"/>
+                    <p class="name">{{catalog}}</p>
+                </div>
+                <div v-for="file in files" class="file">
+                    <img class="icon" :src="fileIcon"/>
                     <p class="name">{{file}}</p>
                 </div>
             </div>
@@ -22,24 +26,36 @@ export default {
     name: 'BrowseView',
     mounted() {
         document.body.style.backgroundImage = `url(${this.background})`;
+        axios.get('./mockFile.json').then((response: any) => {
+            let dirs: string[] = [];
+            let files: string[] = [];
+            for (let i = 0; i < response.data.catalogs.length; i++) {
+                dirs.push(response.data.catalogs[i].name);
+            }
+            for (let i = 0; i < response.data.files.length; i++) {
+                files.push(response.data.files[i].name);
+            }
+            this.files = files;
+            this.catalogs = dirs;
+        });
+    },
+    data() {
+        return {
+            catalogs: [] as string[],
+            files: [] as string[],
+        }
     },
     methods: {
-        getCatalogs(): string[] | void {
-            axios.get('./mockFile.json').then((response: any) => {
-                let dirs: string[] = [];
-                for (let i = 0; i < response.data.catalogs.length; i++) {
-                    dirs.push(response.data.catalogs[i].name);
-                }
-                return dirs;
-            });
-        }
     },
     computed: {
         background(): string {
             return "https://source.unsplash.com/random/1920x1080";
         },
-        icon(): string {
+        folderIcon(): string {
             return "https://cdn-icons-png.flaticon.com/512/5994/5994710.png"
+        },
+        fileIcon(): string {
+            return "https://freesvg.org/img/Anonymous_Paper_2_icon.png"
         },
         fileList(): string[] {
             return ["home", "user", "folder", "dir"];
